2ada629ca7dd44c34d84193af8d2da259ed0a017
Darrell <ridwanakinfenwa001@gmail.com>
2025-02-03 00:03:18 +0100

    Python Day 3 App.py

--- First Project/App.py
+++ First Project/App.py
@@ -21,4 +21,41 @@
 # kilogram = int(user_weight) * 0.4536
 # print(kilogram)
 
-course 
+# course = '''
+# Hi John,
+# Here's a testing email
+# Thanks
+# '''
+# print(course)
+
+# course = 'Testing index numbers'
+# print(course[0])
+# print(course[-1])
+# print(course[0:3)
+# name = 'Jennifer'
+# print(name[1:-1])
+
+# course = 'Python for Beginners'
+# another = course[:]
+# print(another)
+
+
+# formatted strings
+
+# first = 'John'
+# last = 'Smith'
+# message = f'{first} {last} is a coder'
+# print(message)
+
+# String Length
+name = 'Odusote is a man'
+# print(len(name))
+# print(name.upper())
+# print(name.lower())
+# print(name.replace('is a man', 'is a Professor'))
+print(name.replace('Odusote', 'Adekunle'))
+
+# Boolean
+# name = 'Oreoluwa is a girl'
+# print('girl' in name)
+# print('boy' in name)
