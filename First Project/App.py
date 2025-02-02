82c60d25fe0b0cdf140207a974c3247f5a98c811
Darrell <ridwanakinfenwa001@gmail.com>
2025-02-02 01:03:19 +0100

    Add files via upload

---
+++ First Project/App.py
@@ -0,0 +1,24 @@
+# print("I am Zanga")
+# print('o----')
+# print(' ||||')
+# print('*' * 10)
+#
+# patient_fullname = 'Silicon'
+# patient_age = 20
+# is_new = True
+
+# name = input('What is your name? ')
+# colour = input('what is your favourite color? ')
+# print(name + ' likes ' + colour)
+
+# birth_year = input('Birth Year: ')
+# print(type(birth_year))
+# age = 2019 - int(birth_year)
+# print(type(age))
+# print(age)
+
+# user_weight = input('Weight (lbs): ')
+# kilogram = int(user_weight) * 0.4536
+# print(kilogram)
+
+course 
