328564178280a0f0bbd30f64f9a8b30fefb38a7a
Darrell <ridwanakinfenwa001@gmail.com>
2025-02-03 14:21:25 +0100

    Update App.py

--- First Project/App.py
+++ First Project/App.py
@@ -6,6 +6,7 @@
 # patient_fullname = 'Silicon'
 # patient_age = 20
 # is_new = True
+from operator import truediv
 
 # name = input('What is your name? ')
 # colour = input('what is your favourite color? ')
@@ -48,14 +49,91 @@
 # print(message)
 
 # String Length
-name = 'Odusote is a man'
-# print(len(name))
-# print(name.upper())
-# print(name.lower())
+# name = 'Odusote is a man'
+# print(len(name)) count number of characters
+# print(name.upper()) uppercase
+# print(name.lower()) lowercase
 # print(name.replace('is a man', 'is a Professor'))
-print(name.replace('Odusote', 'Adekunle'))
+# print(name.replace('Odusote', 'Adekunle'))
 
 # Boolean
 # name = 'Oreoluwa is a girl'
 # print('girl' in name)
 # print('boy' in name)
+
+# Arithmetic Operations
+# print(10 * 3)
+# x = 10 + 3 * 2 ** 2
+# print(x)
+# exponentiation 2 ** 3
+# multiplication or division
+# addition or subtraction
+
+# Math Function
+# x = 2.9
+# print(round(x))
+# print(abs(-2.9)) // Always returns a positive number
+
+# If Statements
+# is_hot = False
+# is_cold = False
+#
+# if is_hot:
+#     print("It's a hot day")
+#     print("Drink plenty of water")
+# elif is_cold:
+#     print("It's a cold day")
+#     print("Wear warm clothes")
+# else:
+#     print("It's a Lovely day")
+#
+# print('Enjoy your day')
+
+# Exercise
+# good_credit = True
+# bad_credit = False
+#
+# if good_credit:
+#     print("Pay 10%")
+# elif bad_credit:
+#     print("Pay 20%")
+# else:
+#     print("Pay the down payment")
+
+# # Solution
+# price = 1000000
+# has_good_credit = False
+#
+# if has_good_credit:
+#     down_payment = 0.1 * price
+# else:
+#     down_payment = 0.2 * price
+# print(f"Down Payment is: ${down_payment}")
+
+
+# # has_high_income = False
+# has_good_credit = True
+# has_criminal_record = False
+#
+# # if has_high_income and has_good_credit:
+# #     print("Eligible for Loan")
+#
+# # if has_high_income or has_good_credit:
+# #     print("Eligible for Loan")
+#
+# if has_good_credit and not has_criminal_record:
+#     print("Eligible for Loan")
+#
+# # AND: both condition should be true
+# # OR: at least one should be true
+# # NOT: one condition should be false
+
+
+name = "Rid"
+
+if len(name) < 3:
+    print("name must be at least 3 characters")
+elif len(name) > 50:
+    print("name can be a maximum of 50 characters")
+else:
+    print("name looks good!")
