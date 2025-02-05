dd4e54c89a157c6e9b4de3986f9be24f567ec036
Darrell <ridwanakinfenwa001@gmail.com>
2025-02-05 04:28:10 +0100

    Update App.py

--- First Project/App.py
+++ First Project/App.py
@@ -176,3 +176,31 @@ print(f"Total: {total}")
 for x in range(4):
     for y in range(3):
         print(f"({x}, {y})")
+
+
+numbers = [5, 2, 5, 2, 2]
+for x_count in numbers:
+    print('x' * x_count)
+
+numbers = [5, 2, 5, 2, 2]
+for x_count in numbers:
+    output = ''
+    for count in range(x_count):
+        output += 'x'
+    print(output)
+
+
+
+## List
+names = ['John', 'Ridwan', 'Dare', 'Seun']
+names[0] = 'Jon'
+print(names[1:2])
+print(names[2])
+print(names)
+
+numbers = [3, 6, 2, 8, 4, 20]
+max = numbers[0]
+for number in numbers:
+    if number > max:
+        max = number
+print(max)
