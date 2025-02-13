034101dc676c1bb56f3fc20ac407307a7cdffb0e
Darrell <ridwanakinfenwa001@gmail.com>
2025-02-13 06:48:03 +0100

    Update App.py

--- First Project/App.py
+++ First Project/App.py
@@ -343,20 +343,20 @@ from operator import truediv
 
 
 # Classes
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def move(self):
-        print(move)
-
-    def draw(self):
-        print(draw)
-
-point = Point(10, 20)
-point.x = 50
-print(point.x)
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+#
+#     def move(self):
+#         print(move)
+#
+#     def draw(self):
+#         print(draw)
+#
+# point = Point(10, 20)
+# point.x = 50
+# print(point.x)
 
 # point1 = Point()
 # point1.x = 10
@@ -368,6 +368,19 @@ print(point.x)
 # print(point2.x)
 
 
+# Exercise
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def talk(self):
+        print(f"Hi, I am {self.name}")
+
+john = Person("John Doe")
+john.talk()
+
+bob = Person("Bob Smith")
+bob.talk()
 
 
 
